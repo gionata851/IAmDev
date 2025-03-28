7cdb5542ca4cf4ba1fcb1fca002ecc7a4489f176
gionata851 <gionata.prinzo@gmail.com>
2025-03-28 01:22:17 +0100

    ultimi ritocchi

--- dataseeds.ts
+++ dataseeds.ts
@@ -2,22 +2,28 @@ import { PrismaClient } from "./generated/prisma";
 
 let db: PrismaClient = new PrismaClient();
 
-db.book.createMany({
-    data: [
-        {
-            "title": "1984",
-            "author": "George Orwell",
-            "year": 1948
-        },
-        {
-            "title": "L'Insostenibile Leggerezza Dell'Essere",
-            "author": "Milan Kundera",
-            "year": 1984
-        },
-        {
-            "title": "Il Deserto dei Tartari",
-            "author": "Dino Buzzati",
-            "year": 1940
-        }
-    ]
-})
+(async function dataseed() {
+
+    const result = await db.book.createMany({
+        data: [
+            {
+                "title": "1984",
+                "author": "George Orwell",
+                "year": 1948
+            },
+            {
+                "title": "L'Insostenibile Leggerezza Dell'Essere",
+                "author": "Milan Kundera",
+                "year": 1984
+            },
+            {
+                "title": "Il Deserto dei Tartari",
+                "author": "Dino Buzzati",
+                "year": 1940
+            }
+        ]
+    })
+
+    console.log(result);
+})();
+
--- logger/logger.ts
+++ logger/logger.ts
@@ -2,7 +2,6 @@ import { NextFunction, Request, Response } from "express";
 
 export const logger: (req: Request, res: Response, next: NextFunction) => void = (req, res, next) => {
     console.log(`Chiamata con verbo ${req.method}`);
-    console.log(`Parametri ${JSON.stringify(req.params)}`);
     console.log(`Body: ${JSON.stringify(req.body)}`);
     next();
 };
